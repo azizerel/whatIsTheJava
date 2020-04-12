73e48a498f6e342964a02af2a3d7b011f6c5ca43
Abdulaziz Erel <azizerel69@gmail.com>
2020-04-12 21:18:52 +0300

    sql file added for init.

---
+++ javablog/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO test.blog_article(id, created_at, updated_at, version, article_date, author, caption, content)
+values (1,'11-11-2016','11-11-2016',0,'11-11-2016','azizerel','selam','adasda');
